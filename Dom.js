fcc8df5d4f4e5f432152b0ee439d48b11ce2f6f3
Miguel Riaza Valverde <miguelriazavalverde@gmail.com>
2017-01-23 12:07:07 +0100

    Update Dom.js

--- Dom.js
+++ Dom.js
@@ -1,1 +1,34 @@
 
+function createElement(options) {
+	options = options || {};
+	
+	var element;
+	if(options.element) {
+		element = document.createElement(options.element);
+	} else {
+		element = document.createElement("div");
+	}
+	
+	if(options.attr) {
+		for(var prop in options.attr) {
+			element.setAttribute(prop, options.attr[prop]);
+		}
+	}
+	
+	if(options.text) {
+		element.appendChild( document.createTextNode(options.text) );
+	}
+	
+	if(options.children) {
+		var length = options.children.length;
+		for(var i = 0; i < length; i++) {
+			element.appendChild(options.children[i]);
+		}
+	}
+	
+	if(options.parent) {
+		options.parent.appendChild(element);
+	}
+	
+	return element;
+}
