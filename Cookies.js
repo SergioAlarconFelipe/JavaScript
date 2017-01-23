d4844da8ff29e2fa353155d01082859d6bcc5337
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-01-23 09:12:51 +0100

    Create Cookies.js

---
+++ Cookies.js
@@ -0,0 +1,23 @@
+function setCookie (nombre, valor) {
+    document.cookie = nombre + "=" + valor + ";";
+}
+
+function getCookie (nombre) {
+    var name = nombre + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function delCookie (nombre) {
+    document.cookie = nombre + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+}
