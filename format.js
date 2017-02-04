8e4ab37db9be147a1d06d299be1fe73e36bf7f8a
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-02-04 17:47:04 +0100

    Update format.js

--- format.js
+++ format.js
@@ -2,3 +2,7 @@ function formatCeros (num, places) {
     var zero = places - num.toString().length + 1;
     return Array(+(zero > 0 && zero)).join("0") + num;
 }
+
+function uppercaseFirstLetter (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
