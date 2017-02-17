b5f6a374144b3904ca4f8a5c7629c3510d269e01
Miguel Riaza Valverde <miguelriazavalverde@gmail.com>
2017-02-17 12:38:38 +0100

    Update Functions.js

--- Sections/Functions.js
+++ Sections/Functions.js
@@ -1,1 +1,15 @@
+Function.prototype.getNamesParameters = function() {
+    return getNamesParameters(this);
+};
 
+
+var getNamesParameters = (function() {
+    
+    var regex_parameters = /\((.*?)\)/;
+    var regex_spaces = /\s*/g;
+
+    return function(fn) {
+        return regex_parameters.exec(fn.toString())[1].split(",");
+    };
+    
+})();
