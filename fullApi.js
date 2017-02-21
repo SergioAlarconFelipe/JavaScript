36f12f43828ec962d26e4972e8657b6670679489
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-02-21 11:12:50 +0100

    Update fullApi.js

--- fullApi.js
+++ fullApi.js
@@ -76,68 +76,38 @@ function getCookie (name) {
     return "";
 }
 
+function getAllCookies () {
+    var names = [];
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split('; ');
+    for(var i = 0; i <ca.length; i++) {
+        names.push(ca[i].split("=")[0].trim());
+    }
+    return names;
+}
+
 function delCookie (name) {
     document.cookie = name.trim() + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+function delAllCookies () {
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split('; ');
+    for(var i = 0; i <ca.length; i++) {
+        document.cookie = ca[i].split("=")[0].trim() + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    }
 }
 
 function checkCookie (name) {
     var name = name.trim() + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
+    var ca = decodedCookie.split('; ');
     for(var i = 0; i <ca.length; i++) {
-        if (ca[i].indexOf(name) !== -1) {
+        if (ca[i].indexOf(name) === 0) {
             return true;
         }
     }
     return false;
-}
-
-/* API Dom */
-function createElement(options) {
-	options = options || {};
-	
-	var element;
-	if(options.element) {
-		element = document.createElement(options.element);
-	} else {
-		element = document.createElement("div");
-	}
-	
-	if(options.attr) {
-		for(var prop in options.attr) {
-			element.setAttribute(prop, options.attr[prop]);
-		}
-	}
-	
-	if(options.text) {
-		element.appendChild( document.createTextNode(options.text) );
-	}
-	
-	if(options.children) {
-		var length = options.children.length;
-		for(var i = 0; i < length; i++) {
-			if(options.children[i] instanceof HTMLElement) element.appendChild(options.children[i]);
-            		else element.appendChild( createElement(options.children[i]) );
-		}
-	}
-	
-	if(options.parent) {
-		options.parent.appendChild(element);
-	}
-	
-	if(options.style) {
-		for(var style in options.style) {
-			element.style[style] = options.style[style];
-		}
-	}
-
-	if(options.event) {
-		for(var name in options.event) {
-			element.addEventListener(name, options.event[name]);
-		}
-	}
-	
-	return element;
 }
 
 /* API Objects */
