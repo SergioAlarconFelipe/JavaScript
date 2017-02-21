58f6bb3b1fd273e4e122f9167015cfe69c5a8efa
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-02-21 12:05:53 +0100

    Update fullApi.js

--- fullApi.js
+++ fullApi.js
@@ -56,58 +56,60 @@ var ajax = (function () {
 })();
 
 /* API Cookies */
-function setCookie (name, value) {
-    document.cookie = name.trim() + "=" + value + "; path=/";
-}
-
-function getCookie (name) {
-    var name = name.trim() + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
+var cookie = {
+    setCookie: function (name, value) {
+        document.cookie = name.trim() + "=" + value + "; path=/";
+    },
+    
+    getCookie: function (name) {
+        var name = name.trim() + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
         }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
+        return "";
+    },
+
+    getAllCookies: function  () {
+        var names = [];
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split('; ');
+        for(var i = 0; i <ca.length; i++) {
+            names.push(ca[i].split("=")[0].trim());
         }
-    }
-    return "";
-}
+        return names;
+    },
 
-function getAllCookies () {
-    var names = [];
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split('; ');
-    for(var i = 0; i <ca.length; i++) {
-        names.push(ca[i].split("=")[0].trim());
-    }
-    return names;
-}
+    delCookie: function  (name) {
+        document.cookie = name.trim() + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    },
 
-function delCookie (name) {
-    document.cookie = name.trim() + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
-
-function delAllCookies () {
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split('; ');
-    for(var i = 0; i <ca.length; i++) {
-        document.cookie = ca[i].split("=")[0].trim() + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    }
-}
-
-function checkCookie (name) {
-    var name = name.trim() + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split('; ');
-    for(var i = 0; i <ca.length; i++) {
-        if (ca[i].indexOf(name) === 0) {
-            return true;
+    delAllCookies: function  () {
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split('; ');
+        for(var i = 0; i <ca.length; i++) {
+            document.cookie = ca[i].split("=")[0].trim() + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         }
+    },
+
+    checkCookie: function  (name) {
+        var name = name.trim() + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split('; ');
+        for(var i = 0; i <ca.length; i++) {
+            if (ca[i].indexOf(name) === 0) {
+                return true;
+            }
+        }
+        return false;
     }
-    return false;
 }
 
 /* API Objects */
