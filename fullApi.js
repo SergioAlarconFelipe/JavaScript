cbbdaf930c8998be6390ba729de0ea2180e6d544
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-02-22 12:06:13 +0100

    Update fullApi.js

--- fullApi.js
+++ fullApi.js
@@ -56,61 +56,76 @@ var ajax = (function () {
 })();
 
 /* API Cookies */
-var cookie = {
-    setCookie: function (name, value) {
-        document.cookie = name.trim() + "=" + value + "; path=/";
-    },
-    
-    getCookie: function (name) {
-        var name = name.trim() + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for(var i = 0; i <ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    },
+(function () {
+	function cookie (name, value) {
+		if (value) {
+			return cookie.setCookie (name, value);    
+		} else {
+			return cookie.getCookie (name);
+		}
+	}
 
-    getAllCookies: function  () {
-        var names = [];
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split('; ');
-        for(var i = 0; i <ca.length; i++) {
-            names.push(ca[i].split("=")[0].trim());
-        }
-        return names;
-    },
+	cookie.setCookie = function (name, value) {
+		document.cookie = name.trim() + "=" + value + "; path=/";
+		return value;
+	};
 
-    delCookie: function  (name) {
+	cookie.getCookie = function (name) {
+		var name = name.trim() + "=";
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split(';');
+		for(var i = 0; i <ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return "";
+	};
+	
+	cookie.getAllCookies = function  () {
+		var names = [];
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split('; ');
+		for(var i = 0; i <ca.length; i++) {
+			names.push(ca[i].split("=")[0].trim());
+		}
+		return names;
+	};
+
+    cookie.delCookie = function  (name) {
         document.cookie = name.trim() + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    },
-
-    delAllCookies: function  () {
+    };
+	
+	cookie.delAllCookies = function  () {
         var decodedCookie = decodeURIComponent(document.cookie);
         var ca = decodedCookie.split('; ');
         for(var i = 0; i <ca.length; i++) {
             document.cookie = ca[i].split("=")[0].trim() + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         }
-    },
+    };
 
-    checkCookie: function  (name) {
-        var name = name.trim() + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split('; ');
-        for(var i = 0; i <ca.length; i++) {
-            if (ca[i].indexOf(name) === 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
+	cookie.checkCookie = function  (name) {
+		var name = name.trim() + "=";
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split('; ');
+		for(var i = 0; i <ca.length; i++) {
+			if (ca[i].indexOf(name) === 0) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	cookie.toString = function () {
+		return document.cookie;
+	};
+	
+	window.cookie = cookie;
+})();
 
 /* API Objects */
 function listAllProperties(o, tp = false){
