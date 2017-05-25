da5525bfed520c49921090291607bf509f06cb9e
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-05-25 09:00:52 +0200

    Update Functions.js

--- Sections/Functions.js
+++ Sections/Functions.js
@@ -4,7 +4,6 @@ Function.prototype.getNamesParameters = function() {
 
 
 var getNamesParameters = (function() {
-    
     var regex_parameters = /\((.*?)\)/;
     var regex_spaces = /\s*/g;
 
@@ -13,3 +12,31 @@ var getNamesParameters = (function() {
     };
     
 })();
+
+// Funcion para conocer el navegador en uso
+function checkNavegador() {
+    // Opera 8.0+
+    if ( (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0 ) return "Opera";
+    // Firefox 1.0+
+    else if ( typeof InstallTrigger !== 'undefined' ) return "Firefox";
+    // Safari 3.0+ "[object HTMLElementConstructor]" 
+    else if ( /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification) ) return "Safari 3.0+";
+    // Internet Explorer 6-11
+    else if ( /*@cc_on!@*/false || !!document.documentMode ) return "Explorer 6-11";
+    // Edge 20+
+    else if ( ! ( /*@cc_on!@*/false || !!document.documentMode ) && !!window.StyleMedia ) return "Edge 20+";
+    // Chrome 1+
+    else if ( !!window.chrome && !!window.chrome.webstore ) return "Chrome";
+    else return "Otro";
+}
+
+// Funcion para conocer el sistema operativo en uso
+function checkSO() {  
+    var navInfo = window.navigator.appVersion.toLowerCase();  
+    var so = 'Sistema Operativo'; 
+
+    if(navInfo.indexOf('win') != -1) return 'Windows';
+    else if(navInfo.indexOf('linux') != -1) return 'Linux';
+    else if(navInfo.indexOf('mac') != -1) return 'Macintosh';
+    else return "Otro";
+}  
