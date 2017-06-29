de856316f23b01904e65f25554ea0e7ce4feec56
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-06-29 13:45:16 +0200

    Update format.js

--- Sections/format.js
+++ Sections/format.js
@@ -6,3 +6,36 @@ function formatCeros (num, places) {
 function uppercaseFirstLetter (string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function remplazo ( s ) {
+	busquedas = [
+		{ busqueda: '<', remplazo: '$lt;' },
+		{ busqueda: '>', remplazo: '$gt;' },
+		{ busqueda: '&', remplazo: '&amp;' },
+		{ busqueda: '"', remplazo: '&quot;' },
+		{ busqueda: 's', remplazo: '&nbsp;' },
+		{ busqueda: '\'', remplazo: '&apos;' },
+		
+		{ busqueda: 'ñ', remplazo: '&ntilde;' },
+		{ busqueda: 'Ñ', remplazo: '&Ntilde;' },
+		{ busqueda: 'á', remplazo: '&aacute;' },
+		{ busqueda: 'é', remplazo: '&eacute;' },
+		{ busqueda: 'í', remplazo: '&iacute;' },
+		{ busqueda: 'ó', remplazo: '&oacute;' },
+		{ busqueda: 'ú', remplazo: '&uacute;' },
+		{ busqueda: 'Á', remplazo: '&Aacute;' },
+		{ busqueda: 'É', remplazo: '&Eacute;' },
+		{ busqueda: 'Í', remplazo: '&Iacute;' },
+		{ busqueda: 'Ó', remplazo: '&Oacute;' },
+		{ busqueda: 'Ú', remplazo: '&Uacute;' },
+		{ busqueda: '€', remplazo: '&euro;' }
+	];
+	
+	busquedas.forEach( function (e) {
+		patron = new RegExp( "\\" + e.busqueda, "g" )
+		s = s.replace( patron, e.remplazo );
+	} );
+	
+	return s;
+}
+remplazo ( '<a href="pwd"><img src="pwd" /></a>' );
