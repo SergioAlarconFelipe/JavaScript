b66729de8f1dcf41eeb38934b98c048889f59abd
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2018-05-20 00:26:20 +0200

    Update translateP.js

--- Plugins/translatePlugin/js/translateP.js
+++ Plugins/translatePlugin/js/translateP.js
@@ -1,43 +1,34 @@
-(function ( $ ) {
-	/**
-	 *	Function to translate text
-	 */
-	$.translate = 
-		function ( opt ) {
-			$( '[data-translate]' ).translate( opt );
-		}
-	
-    $.fn.translate = 
-		function ( opt ) {
-			// Default opt
-			/*
-			if( typeof( opt ) === 'object' ) {
-				if( opt.lang === undefined ) {
-					opt.lang = 'en';
-				}
-			} else {
-				if( typeof( opt ) === 'string' ) {
-					optAux = opt;
-					opt = {};
-					opt.lang = optAux;
-				} else {
-					opt = {};
-					opt.lang = 'en';
-				}
-			}
-			*/
-			var settings = $.extend( {
-				lang: 'en'
-			}, opt );
-			
-			element = this;			
-			$.get( 'locale/' + settings.lang + '.js', function( res ) {				
-				$( element ).each( function( index, element ) {
-					$( element ).text( translate[ $( element ).attr( 'data-translate' ) ] );
-				} );
-			} );
-			
-			return this;
-		}
-		
-}( jQuery ));
+/**
+ * Function to translate text
+ * 
+ * @author  Sergio
+ * 
+ * @returns this
+ */
+( function( $ ) {
+    $.translate =
+        function( opt ) {
+            $( '[data-translate]' ).translate( opt );
+        };
+
+    $.fn.translate =
+        function( opt ) {
+            // Default opt
+            var settings = $.extend( {
+                lang: 'en',
+                path: 'locale'
+            }, opt );
+            
+            // Funcionalidad
+            element = this;
+            $.get( settings.path + '/' + settings.lang + '.js', function( res ) {
+                $( element ).each( function( index, element ) {
+                    $( element ).text(translate[ $( element ).attr( 'data-translate' ) ] );
+                    $( element ).removeAttr( 'data-translate' );
+                });
+            });
+
+            return this;
+        };
+
+}( jQuery ) );
