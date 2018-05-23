b6857d9c3858334c34ba28c78eb61f14b7eb2ef2
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2018-05-23 17:52:02 +0200

    Create methodsPlugin.js

---
+++ Plugins/expamplesPlugin/methodsPlugin.js
@@ -0,0 +1,49 @@
+( function( $ ){
+
+    var methods = {
+        init: function(options) {
+			console.log('init');
+        },
+        show: function( ) { 
+			console.log('show'); 
+			return this; 
+		},
+        hide: function( ) { 
+			console.log('hide'); 
+			return this; 
+		},
+        update1: function( content ) { 
+			console.log('update'); 
+			console.log(content); 
+			return this; 
+		},
+        update2: function( content1, content2 ) { 
+			console.log('update'); 
+			console.log(content1); 
+			console.log(content2); 
+			return this;
+		},
+		call_private: function( content ) {
+			f = methods['_private'];
+			f.call( this, content );
+		},
+		_private: function( content ) {
+			console.log('private');
+			console.log(content); 
+			return this;
+		},
+    };
+
+    $.fn.plugin = function(methodOrOptions) {
+        if ( methods[methodOrOptions] ) {
+            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+            // Default to "init"
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.plugin' );
+        }    
+    };
+
+
+} )( jQuery );
