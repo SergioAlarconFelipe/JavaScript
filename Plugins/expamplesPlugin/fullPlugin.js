ce61a6ca1d333334ef6bd4e86ad677404440630c
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2018-05-29 17:18:42 +0200

    Update fullPlugin.js

--- Plugins/expamplesPlugin/fullPlugin.js
+++ Plugins/expamplesPlugin/fullPlugin.js
@@ -1,59 +1,66 @@
 ( function( $ ){
-    "use strict";
-
-    var methods = {
+    var methodsPublic = {
         init: function(options) {
-            console.log('init');
+            console.log( 'init' );
             $( this ).data( 'prop', 'val' );
         },
         show: function( ) { 
-            console.log('show'); 
+            console.log( 'show' ); 
             return this; 
         },
         hide: function( ) { 
-            console.log('hide'); 
+            console.log( 'hide' ); 
             return this; 
         },
-        update1: function( content ) { 
-            console.log('update'); 
-            console.log(content); 
+        update1: function( content1 ) { 
+            console.log( 'update' ); 
+            console.log( content1 ); 
             return this; 
         },
         update2: function( content1, content2 ) { 
-            console.log('update'); 
-            console.log(content1); 
-            console.log(content2); 
+            console.log( 'update' ); 
+            console.log( content1 ); 
+            console.log( content2 ); 
             return this;
         },
-        call_private: function( content ) {
-            f = methods['_private'];
-            f.call( this, content );
-        },
-        _private: function( content ) {
-            console.log('private');
-            console.log(content); 
-            return this;
+        call_private: function( content1, content2 ) {
+			console.log( 'call_private' );
+            var f = methodsPrivate[ '_private' ];
+            f.call( this, content1, content2 );
         },
         data: function( ) {
+			console.log( 'data' )
             console.log( $( this ).data( 'prop' ) );
-        }
+        },
     };
-
+	
+	var methodsPrivate = {
+        _private: function( content1, content2 ) {
+            console.log( '_private' );
+            console.log( content1 ); 
+            console.log( content2 ); 
+            return this;
+        },
+	}
+	
     $.fn.plugin = function(methodOrOptions) {
-
-        if ( methods[methodOrOptions] ) {
-            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		if ( methodsPublic[ methodOrOptions ] ) {			
+            //return methodsPublic[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
+			methodsPublic[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
         } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
             // Default to "init"
-            return methods.init.apply( this, arguments );
+            //return methodsPublic.init.apply( this, arguments );
+			methodsPublic.init.apply( this, arguments );
         } else {
             $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.plugin' );
-        }    
+        } 
+		return this;
     };
 
     $.plugin = function(methodOrOptions) {
-        $( 'h1' ).plugin(methodOrOptions);
-
+		var e = $( 'h2' );
+        $( e ).plugin.apply( this, arguments );
+		return e;
     }
-
+	
 } )( jQuery );
