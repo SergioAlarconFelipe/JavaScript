bf02b25c91ef949bba866d307bed2adf317df88d
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2018-05-29 17:57:04 +0200

    Update fullPlugin.js

--- Plugins/expamplesPlugin/fullPlugin.js
+++ Plugins/expamplesPlugin/fullPlugin.js
@@ -31,6 +31,7 @@
         data: function( ) {
             console.log( 'data' );
             console.log( $( this ).data( 'prop' ) );
+			return this;
         },
     };
     
@@ -46,15 +47,14 @@
     $.fn.plugin = function(methodOrOptions) {
         if ( methodsPublic[ methodOrOptions ] ) {			
             //return methodsPublic[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
-            methodsPublic[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
+            return methodsPublic[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
         } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
             // Default to "init"
             //return methodsPublic.init.apply( this, arguments );
-            methodsPublic.init.apply( this, arguments );
+            return methodsPublic.init.apply( this, arguments );
         } else {
             $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.plugin' );
         } 
-        return this;
     };
     
     $.plugin = function(methodOrOptions) {
