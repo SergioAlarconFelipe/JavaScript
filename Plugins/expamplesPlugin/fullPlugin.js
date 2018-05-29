b357c8c64df59697588bba653c217072f6a48812
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2018-05-29 17:55:41 +0200

    Update fullPlugin.js

--- Plugins/expamplesPlugin/fullPlugin.js
+++ Plugins/expamplesPlugin/fullPlugin.js
@@ -2,7 +2,7 @@
     var methodsPublic = {
         init: function(options) {
             console.log( 'init' );
-			$( this ).data( 'prop', 'val' );
+            $( this ).data( 'prop', 'val' );
         },
         show: function( ) { 
             console.log( 'show' ); 
@@ -24,42 +24,42 @@
             return this;
         },
         call_private: function( content1, content2 ) {
-			console.log( 'call_private' );
+            console.log( 'call_private' );
             var f = methodsPrivate[ '_private' ];
             f.call( this, content1, content2 );
         },
         data: function( ) {
-			console.log( 'data' );
+            console.log( 'data' );
             console.log( $( this ).data( 'prop' ) );
         },
     };
-	
-	var methodsPrivate = {
+    
+    var methodsPrivate = {
         _private: function( content1, content2 ) {
             console.log( '_private' );
             console.log( content1 ); 
             console.log( content2 ); 
             return this;
         },
-	}
-	
+    }
+    
     $.fn.plugin = function(methodOrOptions) {
-		if ( methodsPublic[ methodOrOptions ] ) {			
+        if ( methodsPublic[ methodOrOptions ] ) {			
             //return methodsPublic[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
-			methodsPublic[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
+            methodsPublic[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
         } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
             // Default to "init"
             //return methodsPublic.init.apply( this, arguments );
-			methodsPublic.init.apply( this, arguments );
+            methodsPublic.init.apply( this, arguments );
         } else {
             $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.plugin' );
         } 
-		return this;
+        return this;
     };
-
+    
     $.plugin = function(methodOrOptions) {
-		var e = $( 'h2' );
+        var e = $( 'p' );
         return $( e ).plugin.apply( e, arguments );
     }
-	
+    
 } )( jQuery );
