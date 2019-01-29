8beca6dff540ca05169ded22c5c44d2fb8e6ce38
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2019-01-29 09:53:10 +0100

    Update Objects.js

--- Sections/Objects.js
+++ Sections/Objects.js
@@ -18,3 +18,23 @@ function listAllProperties(o, tp = false){
       return Object.keys (o);
    }
 }
+
+// Order objs by property
+data = {
+	1: { n: 'n1', orden: 2 },
+	2: { n: 'n2', orden: 1 },
+}
+
+function compare( a, b ) {
+	if( a.orden < b.orden )
+		return -1;
+	if( a.orden > b.orden )
+		return 1;
+	return 0;
+}
+function compare( a, b ) {
+	return a.orden - b.orden;
+}
+
+array = Object.values( data ).sort( compare );
+Object.assign( {}, array );
