e60f556d54609e4481b736badec302f0644b8159
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2020-06-19 12:05:59 +0200

    Update Functions.js

--- Sections/Functions.js
+++ Sections/Functions.js
@@ -365,3 +365,79 @@ console.log( create_UUID( opts ) );
 //	"170c684f840|3995fc|id_sub"
 //	"170c6851780|3c8fff|id_sub"
 //	"170c6855600|4cf6e8|id_sub"
+
+// Funcion lanzar eventos swipe en desktop y movil
+( function( element ) {
+    element.addEventListener('touchstart', start, false);
+    element.addEventListener('touchmove', move, false); 
+    element.addEventListener('touchend', end, false);
+
+    element.addEventListener('mousedown', start, false);
+    element.addEventListener('mousemove', move, false);
+    element.addEventListener('mouseup', end, false);
+
+    var pxSensibility = 100;
+    var touching = false;
+    var direction = 'none';
+    var touchX = false;
+
+    function start( event ) {
+        touching = true;
+        touchX = event.x;
+    }
+    function end( event ) {
+        touching = false;
+        direction = 'none';
+    }
+    function move( event ) {
+        if( touching ) {
+            if( direction === 'none' ) {
+                if( event.x > touchX ) {
+                    direction = 'right';
+                }
+                else if( event.x < touchX ) {
+                    direction = 'left';
+                }
+            }
+
+            if( direction === 'right' && event.x < touchX ) {
+                touching = false;
+                direction = 'none';
+            }
+            else if( direction === 'left' && event.x > touchX ) {
+                touching = false;
+                direction = 'none';
+            }
+
+            if( event.x >= touchX + pxSensibility ) {
+                touching = false;
+                direction = 'none';
+                
+                var cE = new CustomEvent(
+                    "swiperight", 
+                    {
+                        bubbles: true,
+                        cancelable: true
+                    }
+                );
+                event.srcElement.dispatchEvent( cE );
+            }
+            else if( event.x <= touchX - pxSensibility ) {
+                touching = false;
+                direction = 'none';
+                
+                var cE = new CustomEvent(
+                    "swipeleft", 
+                    {
+                        bubbles: true,
+                        cancelable: true
+                    }
+                );
+                event.srcElement.dispatchEvent( cE );
+            }
+        }
+    }
+} )( document );
+
+document.getElementById( 'h2' ).addEventListener('swiperight', function( event ) { console.log( 1, event ); }, false);
+document.getElementById( 'h2' ).addEventListener('swipeleft', function( event ) { console.log( 2, event ); }, false);
